dd419b53c958e0f0709984418fcfb831f259edd1
Omari Domville <you@example.com>
2020-03-25 15:44:36 -0500

    update db connection

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,8 @@ UPLOAD_FOLDER = './app/static/uploads'
 # Config Values
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://sohydtnhzkmbac:ba3db9d466e5aa0964683e57bb9c8020c8e1adc258a7a3301507185be987a756@ec2-18-235-97-230.compute-1.amazonaws.com:5432/deied3ba3oa5to"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://poeajwhhuqcnon:87603677de0c8650de443b84e82c9456a515a9444648318a4b1f3ab034c75f2d@ec2-18-210-51-239.compute-1.amazonaws.com:5432/d49pc8v7mhi70f"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://sohydtnhzkmbac:ba3db9d466e5aa0964683e57bb9c8020c8e1adc258a7a3301507185be987a756@ec2-18-235-97-230.compute-1.amazonaws.com:5432/deied3ba3oa5to"
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 SECRET_KEY = 'Sup3r$3cretkey'
